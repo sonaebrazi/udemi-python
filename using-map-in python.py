fac3ae43926573f749a55e39a5e8a4f1a77057f2
Sona <ebrazi.sona@gmail.com>
2022-04-21 15:00:11 +0200

    map in python

--- using-map-in python.py
+++ using-map-in python.py
@@ -2,6 +2,7 @@ one_to_ten = range (1 , 11)
 def double_num(num):
     return num * 2
 print(list(map(double_num , one_to_ten)))
+
 print(list(map((lambda x: x*3) , one_to_ten)))
 
 a_list = list(map((lambda x , y : x+y) ,  [1 ,2 , 3],[1 ,2 , 3]))
