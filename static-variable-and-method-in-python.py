2cc9a89e594a7d5d6e417d971286d1ec0af79df5
Sona <ebrazi.sona@gmail.com>
2022-04-21 09:22:51 +0200

    static variable and method

---
+++ static-variable-and-method-in-python.py
@@ -0,0 +1,18 @@
+class Dog:
+    num_of_dogs = 0
+    def __init__(self , name="unknown"):
+        self.name = name
+        Dog.num_of_dogs += 1
+
+    @staticmethod
+    def get_num_of_dogs():
+        print("There are currently {} dogs".format(Dog.num_of_dogs))
+
+def main():
+    spot = Dog("spot")
+    greg = Dog("greg")
+    Dog.get_num_of_dogs()
+    spot.get_num_of_dogs()
+
+main()
+
